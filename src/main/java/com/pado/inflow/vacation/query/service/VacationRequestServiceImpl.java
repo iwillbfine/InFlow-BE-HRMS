8f78be255cab333ce023d30a4e6ebacd1ffd7b2c
woodart8 <kibeom1145@gmail.com>
2024-11-11 17:05:45 +0900

    Feat: 사원별 휴가 신청 내역 조회 기능 구현

--- src/main/java/com/pado/inflow/vacation/query/service/VacationRequestServiceImpl.java
+++ src/main/java/com/pado/inflow/vacation/query/service/VacationRequestServiceImpl.java
@@ -1,7 +1,60 @@
 package com.pado.inflow.vacation.query.service;
 
+import com.pado.inflow.vacation.query.dto.PageDTO;
+import com.pado.inflow.vacation.query.dto.VacationRequestDTO;
+import com.pado.inflow.vacation.query.repository.VacationRequestMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class VacationRequestServiceImpl {
+public class VacationRequestServiceImpl implements VacationRequestService {
+
+    private final Integer PAGE_SIZE = 10; // 페이지 간격
+    private final Integer ELEMENTS_PER_PAGE = 10; // 한 페이지 당 요소 개수
+
+    private final VacationRequestMapper vacationRequestMapper;
+
+    @Autowired
+    public VacationRequestServiceImpl(VacationRequestMapper vacationRequestMapper) {
+        this.vacationRequestMapper = vacationRequestMapper;
+    }
+
+    // 사원별 휴가 신청 내역 미리보기 조회
+    @Override
+    public List<VacationRequestDTO> findVacationRequestPreviewsByEmployeeId(Long employeeId) {
+        List<VacationRequestDTO> vacationRequestPreviews =
+                vacationRequestMapper.findVacationRequestPreviewsByEmployeeId(employeeId);
+
+//        if (vacationRequestPreviews == null || vacationRequestPreviews.isEmpty()) {
+//            throw new
+//        }
+
+        return vacationRequestPreviews;
+    }
+
+    // 사원별 휴가 신청 내역 전체 조회
+    @Override
+    public PageDTO<VacationRequestDTO> findVacationRequestsByEmployeeId(Long employeeId, Integer pageNo) {
+        // 페이지 번호 유효성 검사
+//        if(pageNo == null || pageNo < 1) {
+//            throw new CommonException(ErrorCode.INVALID_PARAMETER_FORMAT);
+//        }
+
+        Integer totalElements = vacationRequestMapper.getTotalVacationRequestsByEmployeeId(employeeId);
+//        if(totalElements == null || totalElements < 1) {
+//            throw new
+//        }
+
+        Integer offset = (pageNo - 1) * ELEMENTS_PER_PAGE;
+        List<VacationRequestDTO> vacationRequests =
+                vacationRequestMapper.findVacationRequestsByEmployeeId(employeeId, ELEMENTS_PER_PAGE, offset);
+//        if (vacations == null || vacations.isEmpty()) {
+//            throw new
+//        }
+
+        return new PageDTO<>(vacationRequests, pageNo, PAGE_SIZE, ELEMENTS_PER_PAGE, totalElements);
+    }
+
 }
---
+++ src/test/java/com/pado/inflow/vacation/query/service/VacationRequestServiceTests.java
@@ -0,0 +1,58 @@
+package com.pado.inflow.vacation.query.service;
+
+import com.pado.inflow.vacation.query.dto.PageDTO;
+import com.pado.inflow.vacation.query.dto.VacationRequestDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@SpringBootTest
+@Transactional
+class VacationRequestServiceTests {
+
+    @Autowired
+    private VacationRequestService vacationRequestService;
+
+    @DisplayName("사원별 휴가 신청 내역 미리보기 테스트")
+    @Test
+    void testFindVacationRequestPreviewsByEmployeeId() {
+        // Given
+        Long employeeId = 1L;
+
+        // When
+        List<VacationRequestDTO> vacationRequestPreviews =
+                vacationRequestService.findVacationRequestPreviewsByEmployeeId(employeeId);
+        if (vacationRequestPreviews != null && !vacationRequestPreviews.isEmpty()) {
+            vacationRequestPreviews.forEach(x -> log.info(x.toString()));
+        }
+
+        // Then
+        Assertions.assertNotNull(vacationRequestPreviews);
+    }
+
+    @DisplayName("사원별 휴가 신청 내역 전체 조회 테스트")
+    @Test
+    void testFindVacationRequestsByEmployeeId() {
+        // Given
+        Long employeeId = 1L;
+        Integer pageNo = 1;
+
+        // When
+        PageDTO<VacationRequestDTO> vacationRequests =
+                vacationRequestService.findVacationRequestsByEmployeeId(employeeId, pageNo);
+        if (vacationRequests != null) {
+            log.info(vacationRequests.toString());
+        }
+
+        // Then
+        Assertions.assertNotNull(vacationRequests);
+    }
+
+}
