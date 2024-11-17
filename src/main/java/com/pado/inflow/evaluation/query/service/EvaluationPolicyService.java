d98add2d819476ec680c390fe8028f95e29b9e70
mingeun <caolian2003@naver.com>
2024-11-17 21:41:00 +0900

    Feat : 평가정책 조회 기능 구현
    RequestParam을 이용 Year와 Half를 받아
    특정 년도 및 반기에 해당하는 정책 전체 조회 기능

---
+++ src/main/java/com/pado/inflow/evaluation/query/service/EvaluationPolicyService.java
@@ -0,0 +1,10 @@
+package com.pado.inflow.evaluation.query.service;
+
+import com.pado.inflow.evaluation.query.dto.EvaluationPolicyDTO;
+
+import java.util.List;
+
+public interface EvaluationPolicyService {
+
+    List<EvaluationPolicyDTO> findPolicyWithYearAndHalf(Integer year, String half);
+}
---
+++ src/main/java/com/pado/inflow/evaluation/query/service/EvaluationPolicyServiceImpl.java
@@ -0,0 +1,33 @@
+package com.pado.inflow.evaluation.query.service;
+
+import com.pado.inflow.common.exception.CommonException;
+import com.pado.inflow.common.exception.ErrorCode;
+import com.pado.inflow.evaluation.query.dto.EvaluationPolicyDTO;
+import com.pado.inflow.evaluation.query.repository.EvaluationPolicyMapper;
+import org.apache.ibatis.ognl.Evaluation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EvaluationPolicyServiceImpl implements EvaluationPolicyService {
+
+    @Autowired
+    private final EvaluationPolicyMapper evaluationPolicyMapper;
+
+    public EvaluationPolicyServiceImpl(EvaluationPolicyMapper evaluationPolicyMapper) {
+        this.evaluationPolicyMapper = evaluationPolicyMapper;
+    }
+
+    @Override
+    public List<EvaluationPolicyDTO> findPolicyWithYearAndHalf(Integer year, String half) {
+        List<EvaluationPolicyDTO> selectedPolicy =
+                evaluationPolicyMapper.findPolicyByYearAndHalf(year, half);
+
+        if (selectedPolicy == null) {
+            throw new CommonException(ErrorCode.NOT_FOUND_EVALUATION_POLICY);
+        }
+        return selectedPolicy;
+    }
+}
