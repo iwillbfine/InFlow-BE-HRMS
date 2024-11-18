7ffdbf7d9c34f8eeac8f0e62ec85bd9621a6f809
mingeun <caolian2003@naver.com>
2024-11-18 11:24:44 +0900

    Refactor : 등급 조회 시 예외처리 구문 추가
    개발 시 등급 조회에 대한 예외처리 구문이 빠져있어서 추가하였습니다.

--- src/main/java/com/pado/inflow/evaluation/query/service/EvaluationServiceImpl.java
+++ src/main/java/com/pado/inflow/evaluation/query/service/EvaluationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pado.inflow.evaluation.query.service;
 
+import com.pado.inflow.common.exception.CommonException;
+import com.pado.inflow.common.exception.ErrorCode;
 import com.pado.inflow.evaluation.query.dto.EvaluationDTO;
 import com.pado.inflow.evaluation.query.repository.EvaluationMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,9 @@ public class EvaluationServiceImpl implements EvaluationService {
     @Override
     public EvaluationDTO findEvaluationGrade(Long empId, Integer year, String half) {
         EvaluationDTO finalGrade = evaluationMapper.getFinalEvaluationGrade(empId, year, half);
+        if (finalGrade == null) {
+            throw new CommonException(ErrorCode.NOT_FOUND_GRADE);
+        }
         return finalGrade;
     }
 }
