75572771e4f52a355445877ca4a82094a1c0adb4
yncayn <julieche3@gmail.com>
2024-11-22 21:40:08 +0900

    Feat: 팀장 내 부서 관리- 부서 코드를 통해 해당 부서 사원 목록 조회 기능 구현

---
+++ src/main/java/com/pado/inflow/department/query/dto/ManagerRoleGetDepartmentMemberListByDepartmentCodeDTO.java
@@ -0,0 +1,32 @@
+package com.pado.inflow.department.query.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ManagerRoleGetDepartmentMemberListByDepartmentCodeDTO {
+
+    // 사원명, 사원 부서경로, 사원코드, 근태상태,
+
+    @JsonProperty("employee_name")
+    private String employeeName;
+
+    @JsonProperty("employee_number")
+    private String employeeNumber;
+
+    @JsonProperty("department_path")
+    private String departmentPath;
+
+    @JsonProperty("attendance_status_type_name")
+    private String attendanceStatusTypeName;
+
+
+
+}
