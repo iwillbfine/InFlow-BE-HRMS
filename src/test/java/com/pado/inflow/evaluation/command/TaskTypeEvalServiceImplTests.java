d2c64a86fb5695eff635bc254bf2660dea734c3e
mingeun <caolian2003@naver.com>
2024-11-26 16:29:52 +0900

    Refactor : 오타 수정

--- src/test/java/com/pado/inflow/evaluation/command/TaskTypeEvalServiceImplTests.java
+++ src/test/java/com/pado/inflow/evaluation/command/TaskTypeEvalServiceImplTests.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class TaskTypeEvalServiceImplTest {
+class TaskTypeEvalServiceImplTests {
 
     @InjectMocks
     private TaskTypeEvalServiceImpl taskTypeEvalService;
