704d82c0ed4e0eee889dfc587bdd3f5dfba0a20f
Haegwan-Choe <kkhagwan@naver.com>
2024-11-26 17:50:54 +0900

    Refactor: @JsonProperty 추가

--- src/main/java/com/pado/inflow/payroll/query/dto/AllPaymentsDTO.java
+++ src/main/java/com/pado/inflow/payroll/query/dto/AllPaymentsDTO.java
@@ -1,5 +1,6 @@
 package com.pado.inflow.payroll.query.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,11 +11,18 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Data
 public class AllPaymentsDTO {
+    @JsonProperty("payment_id")
     private Long paymentId;
+    @JsonProperty("paid_at")
     private LocalDateTime paidAt;
+    @JsonProperty("monthly_salary")
     private int monthlySalary;
+    @JsonProperty("actual_salary")
     private int actualSalary;
+    @JsonProperty("non_taxable_salary")
     private int nonTaxableSalary;
+    @JsonProperty("total_deductible")
     private int totalDeductible;
+    @JsonProperty("employee_id")
     private Long employeeId;
 }
