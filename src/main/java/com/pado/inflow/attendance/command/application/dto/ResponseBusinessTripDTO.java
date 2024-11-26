69dd20fb6509fe9cf2c737fb28639933386f0dcb
Kibeom Jeon <kibeom1145@gmail.com>
2024-11-26 12:58:47 +0900

    Feat: 출근 기능 구현

--- src/main/java/com/pado/inflow/attendance/command/application/dto/ResponseBusinessTripDTO.java
+++ src/main/java/com/pado/inflow/attendance/command/application/dto/ResponseBusinessTripDTO.java
@@ -2,8 +2,7 @@ package com.pado.inflow.attendance.command.application.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.pado.inflow.attendance.command.domain.aggregate.type.OvertimeStatus;
-import com.pado.inflow.attendance.command.domain.aggregate.type.RemoteStatus;
+import com.pado.inflow.attendance.command.domain.aggregate.type.TripType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,12 +15,12 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Data
 @Builder
-public class CommuteDTO {
-    private Long commuteId;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private RemoteStatus remoteStatus;
-    private OvertimeStatus overtimeStatus;
+public class ResponseBusinessTripDTO {
+    private Long businessTripId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private TripType tripType;
+    private String destination;
     private Long employeeId;
     private Long attendanceRequestId;
 }
--- src/main/java/com/pado/inflow/attendance/command/application/service/CommuteCommandService.java
+++ src/main/java/com/pado/inflow/attendance/command/application/service/CommuteCommandService.java
@@ -1,13 +1,10 @@
 package com.pado.inflow.attendance.command.application.service;
 
-import com.pado.inflow.attendance.command.application.dto.CommuteDTO;
+import com.pado.inflow.attendance.command.application.dto.ResponseCommuteDTO;
 
 public interface CommuteCommandService {
 
     // 출근
-    CommuteDTO goToWork(Long employeeId);
-
-    // 재택근무 출근
-    CommuteDTO goToRemoteWork(Long commuteId);
+    String checkAndUpdateCommute(Long employeeId);
 
 }
