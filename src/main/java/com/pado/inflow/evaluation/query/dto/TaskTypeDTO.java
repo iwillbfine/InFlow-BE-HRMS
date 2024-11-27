d3dc768dd88935960cc524158af6b1d2df59a8c1
mingeun <caolian2003@naver.com>
2024-11-27 16:45:36 +0900

    Refactor : DTO에 JsonProperty 추가

--- src/main/java/com/pado/inflow/evaluation/query/dto/TaskTypeDTO.java
+++ src/main/java/com/pado/inflow/evaluation/query/dto/TaskTypeDTO.java
@@ -1,5 +1,6 @@
 package com.pado.inflow.evaluation.query.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TaskTypeDTO {
 
+    @JsonProperty("task_type_id")
     private Long taskTypeId;
+
+    @JsonProperty("task_type_name")
     private String taskTypeName;
 }
