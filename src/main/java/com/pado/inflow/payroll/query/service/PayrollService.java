470e61cebd0821e9aba3ba09fd6412ff04f09f66
Haegwan-Choe <kkhagwan@naver.com>
2024-12-04 16:15:04 +0900

    Feat: 급여 기간별 조회, 연도별 조회 기능 추가

--- src/main/java/com/pado/inflow/payroll/query/service/PayrollService.java
+++ src/main/java/com/pado/inflow/payroll/query/service/PayrollService.java
@@ -4,6 +4,8 @@ import com.pado.inflow.payroll.query.dto.AllPaymentsDTO;
 import com.pado.inflow.payroll.query.dto.PageDTO;
 import com.pado.inflow.payroll.query.dto.PayrollDTO;
 
+import java.util.List;
+
 public interface PayrollService {
 
     // 사원별 연월별 급여 명세서 조회
@@ -11,4 +13,8 @@ public interface PayrollService {
 
     // 사원별 전체 급여 내역 조회
     PageDTO<AllPaymentsDTO> findPaymentsByEmployeeId(Long employeeId, Integer pageNo);
+
+    List<AllPaymentsDTO> findPaymentsByYear(Long employeeId, int year);
+
+    List<AllPaymentsDTO> findPeriodicPayments(Long employeeId, int startMonth, int endMonth);
 }
