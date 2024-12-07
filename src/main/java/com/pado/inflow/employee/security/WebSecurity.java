507d7373ff68e2c47cf9e8104e05a9009a6b9aff
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-08 00:00:27 +0900

    Refactor: health 권한 수정

--- src/main/java/com/pado/inflow/employee/security/WebSecurity.java
+++ src/main/java/com/pado/inflow/employee/security/WebSecurity.java
@@ -63,6 +63,8 @@ public class WebSecurity {
 
         // 설명. 권한 설정
         http.authorizeHttpRequests(authz -> authz
+                        .requestMatchers(new AntPathRequestMatcher("/health", "GET")).permitAll()
+
                         // 설명. 1. 로그인은 어떤 사용자도 이용 가능
                         .requestMatchers(new AntPathRequestMatcher("/api/login", "POST")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/api/auth/**", "POST")).permitAll()
