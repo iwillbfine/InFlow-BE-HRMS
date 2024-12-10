7479928c205cfed00d419d78e8c6784b710752d4
iwillbefine <iwillbfine@naver.com>
2024-12-10 22:48:02 +0900

    Feat: 포상및징계 api 생성

--- src/main/java/com/pado/inflow/employee/security/WebSecurity.java
+++ src/main/java/com/pado/inflow/employee/security/WebSecurity.java
@@ -311,6 +311,7 @@ public class WebSecurity {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
         configuration.setAllowedOrigins(List.of("https://inflow.run")); // Allow frontend
+//        configuration.setAllowedOrigins(List.of("http://localhost:5173/")); // Allow frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*")); // Allow all headers
 
