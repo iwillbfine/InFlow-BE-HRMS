cdfae0affebadd58256d12d020699ff58e6c702d
woodart8 <kibeom1145@gmail.com>
2024-12-11 09:28:12 +0900

    Refactor: 서울 시간대 설정

--- src/main/java/com/pado/inflow/InflowApplication.java
+++ src/main/java/com/pado/inflow/InflowApplication.java
@@ -1,15 +1,27 @@
 package com.pado.inflow;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
+@Slf4j
 public class InflowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InflowApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        // JVM 전역 시간대 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("현재 시스템 시간대: {}", TimeZone.getDefault().getID());
     }
 
 }
